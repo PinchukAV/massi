d1e094375cc2411647e5dfa1e29f1d7981cc1fb1
PinchukAV <angel71.05@mail.ru>
2022-09-17 15:20:43 +0300

    домашнее дз

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int [] numbers = {1,2,3};
+        numbers [0]= 1;
+        int festNunber = numbers[0];
+        System.out.println(festNunber);
+        System.out.println(numbers[0]);
+        System.out.println(numbers[3]);
+        int fest=0;
+        for (int i = 0; i < 3; i++) {
+            System.out.println(numbers[i]);
+        }
     }
 }
